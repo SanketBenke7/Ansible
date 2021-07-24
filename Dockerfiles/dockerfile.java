49859594d84d6b7c1bef4899b25b84d5195ebe0e
sanket_benke <78406532+SanketBenke7@users.noreply.github.com>
2021-07-24 12:37:07 +0530

    Update dockerfile.java

--- Dockerfiles/dockerfile.java
+++ Dockerfiles/dockerfile.java
@@ -4,3 +4,7 @@ RUN apt-get -y update
 RUN apt-get install -y default-jre
 RUN apt-get install -y default-jdk
 ENTRYPOINT ["tail", "-f", "/dev/null"]
+
+
+#docker build --no-cache -t java -f dockerfile.java .
+#docker run -d --name java java
